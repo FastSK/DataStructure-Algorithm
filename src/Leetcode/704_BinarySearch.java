3edac1d1947704bb4cd13b5c6d21f42608e52a0b
FastSK <kimsu@merrimack.edu>
2023-03-01 23:06:55 -0500

    commit

---
+++ src/Leetcode/704_BinarySearch.java
@@ -0,0 +1,24 @@
+//package Leetcode;
+//
+//import java.util.HashMap;
+//import java.util.Map;
+//
+//class Solution {
+//    public int[] twoSum(int[] nums, int target) {
+//        Map<Integer, Integer> map = new HashMap<>();
+//        for (int i = 0; i < nums.length; i++) {
+//            int x = nums[i];
+//            if (map.containsKey(target - x)) {
+//                return new int[]{map.get(target - x), i};
+//            }
+//            map.put(x, i);
+//        }
+//        throw new IllegalArgumentException("No two sum solution");
+//    }
+//}
+//
+//class Solution {
+//    public int search(int[] nums, int target){
+//
+//    }
+//}
